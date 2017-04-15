a933a6f454e91ad261d648fd2c00b889c56885a1
Roshan Padaki <roshanpadaki@gmail.com>
2017-04-15 19:58:48 -0400

    Fixed type errors.

--- kk.cpp
+++ kk.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main() {
 	random_device rd;
-	mt19937 gen(rd());
+	mt19937_64 gen(rd());
 	uniform_int_distribution<uint64_t> dis(1,pow(10,12));
 	for (int i = 0; i < 7; ++i)
 	{
